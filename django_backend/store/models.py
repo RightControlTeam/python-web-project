ae96392bca0d6ecad45963d4be7c25fb01808767
DAMatkov <dm318929@gmail.com>
2026-03-18 22:21:36 +0300

    Add store app and models

---
+++ django_backend/store/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from users.models import User
+
+
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
