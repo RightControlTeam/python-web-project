b51c1b7424ad2df72591ccdbc66a34ebe4c5fd02
DAMatkov <dm318929@gmail.com>
2026-03-24 16:08:17 +0300

    explicit http_method_names and custom permissions for CategoryViewSet

--- django_backend/store/views.py
+++ django_backend/store/views.py
@@ -7,4 +7,13 @@ from .serializers import CategorySerializer
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [permissions.AllowAny]
+    def get_permissions(self):
+        if self.action ==  "create":
+            permission_classes = [permissions.IsAuthenticated]
+        elif self.action == "destroy":
+            permission_classes = [permissions.IsAdminUser]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
+
+    http_method_names = ['get', 'post', 'delete']
