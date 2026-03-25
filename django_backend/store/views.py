1ba55f1be327a66ad99da60e27c441afc3a34e14
Vika-program <borisovav308@gmail.com>
2026-03-25 16:11:03 +0300

    fixed permissions

--- django_backend/store/views.py
+++ django_backend/store/views.py
@@ -28,12 +28,10 @@ class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     def get_permissions(self):
-        if self.action in ["update", "partial_update", "create"]:
-            permission_classes = [permissions.IsAuthenticated]
-        elif self.action == "destroy":
-            permission_classes = [permissions.IsAdminUser]
-        else:
+        if self.action in ["list", "retrieve"]:
             permission_classes = [permissions.AllowAny]
+        else:
+            permission_classes = [permissions.IsAdminUser]
         return [permission() for permission in permission_classes]
 
     http_method_names = ['get', 'post', 'delete', 'put', 'patch']
