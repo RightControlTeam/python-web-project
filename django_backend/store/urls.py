8fe701de93cae1665abaa23ac1fb71f6548da0b4
Vika-program <borisovav308@gmail.com>
2026-03-25 15:44:13 +0300

    add view for product and pyproject.toml

--- django_backend/store/urls.py
+++ django_backend/store/urls.py
@@ -1,11 +1,12 @@
 # store/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet
+from .views import CategoryViewSet, ProductViewSet
 
 router = DefaultRouter()
 
 router.register(r'category', CategoryViewSet)
+router.register(r'product', ProductViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- django_backend/store/views.py
+++ django_backend/store/views.py
@@ -1,8 +1,10 @@
 # store/views.py
 
-from rest_framework import viewsets, permissions
-from .models import Category
-from .serializers import CategorySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, permissions, filters
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -17,3 +19,21 @@ class CategoryViewSet(viewsets.ModelViewSet):
         return [permission() for permission in permission_classes]
 
     http_method_names = ['get', 'post', 'delete']
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
+    filterset_fields = ['category']
+    search_fields = ['name']
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    def get_permissions(self):
+        if self.action in ["update", "partial_update", "create"]:
+            permission_classes = [permissions.IsAuthenticated]
+        elif self.action == "destroy":
+            permission_classes = [permissions.IsAdminUser]
+        else:
+            permission_classes = [permissions.AllowAny]
+        return [permission() for permission in permission_classes]
+
+    http_method_names = ['get', 'post', 'delete', 'put', 'patch']
