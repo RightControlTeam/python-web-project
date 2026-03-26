ac0016379f7e0b9f97c6e19c4915dd91adb873f0
DariaEFAL <EF.Daria.AL@yandex.ru>
2026-03-26 05:53:52 +0300

    feat: add Order in views.py and urls.py

--- django_backend/store/urls.py
+++ django_backend/store/urls.py
@@ -1,13 +1,14 @@
 # store/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoryViewSet, ProductViewSet, CartItemViewSet
+from .views import CategoryViewSet, ProductViewSet, CartItemViewSet, OrderViewSet
 
 router = DefaultRouter()
 
 router.register(r'category', CategoryViewSet)
 router.register(r'product', ProductViewSet)
 router.register(r'cart', CartItemViewSet)
+router.register(r'order', OrderViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
