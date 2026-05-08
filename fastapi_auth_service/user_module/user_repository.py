eb22d62ba383fdfe969704f6a4988336986d0609
DAMatkov <dm318929@gmail.com>
2026-05-08 20:48:10 +0300

    Add basic UserRepository (create, get, get list)

---
+++ fastapi_auth_service/user_module/user_repository.py
@@ -0,0 +1,23 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from user_module.user import User
+from typing import Optional, List
+
+
+class UserRepository:
+    async def get_user_by_id(self, db: AsyncSession, user_id: int) -> Optional[User]:
+        query = select(User).where(User.id == user_id)
+        result = await db.execute(query)
+        return result.scalar_one_or_none()
+
+    async def get_list(self, db: AsyncSession, offset: int = 0, limit: int = 10) -> List[User]:
+        query = select(User).offset(offset).limit(limit)
+        result = await db.execute(query)
+        return list(result.scalars().all())
+
+    async def create(self, db: AsyncSession, new_user: User) -> User:
+        db.add(new_user)
+        await db.commit()
+        await db.refresh(new_user)
+        return new_user
+
