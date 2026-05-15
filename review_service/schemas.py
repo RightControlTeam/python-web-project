9d578a1add1d133b501792b88d667045c6aaf10b
Opik1704 <dsgorch04@gmail.com>
2026-05-15 21:01:30 +0300

    Валидация

--- review_service/schemas.py
+++ review_service/schemas.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class ReviewCreateRequest(BaseModel):
     product_id: int
-    text: str
-    rating: int
+    text: str = Field(..., min_length=5, max_length=1000)
+    rating: int = Field(..., ge=1, le=5)
 
 class ReviewUpdateRequest(BaseModel):
     text: str
