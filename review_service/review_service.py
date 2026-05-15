60665c3317b1908ed0b4270e65db6abbf6284fd4
Opik1704 <dsgorch04@gmail.com>
2026-05-15 21:01:48 +0300

    Сервис отзывов

--- review_service/review_service.py
+++ review_service/review_service.py
@@ -1,42 +1,73 @@
 from typing import Optional, List
 from models import Review
+from database import db
+from review_service.review_repository import ReviewRepository
 from schemas import ReviewCreateRequest, ReviewUpdateRequest
-from review_repository import ReviewRepository
+import httpx
 
 # где-то тут надо будет добавить проверку существования пользователей и товаров
 # обращением к другим сервисам
 
 class ReviewService:
+    DJANGO_API_URL = "http://localhost:8080/api/product/"
+
     @staticmethod
-    def create(review_create_request: ReviewCreateRequest, user_id: int) -> Review:
-        # создаем объект review, добавляем данные из запроса, добавляем id пользователя
-        # id берем из токена
-        ...
+    async def verify_product_exists(product_id: int) -> bool:
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(f"{ReviewService.DJANGO_API_URL}{product_id}/")
+                return response.status_code == 200
+            except httpx.RequestError:
+                return False
+
+    @staticmethod
+    async def create(data, user_id: int) -> Optional[Review]:
+        if not await ReviewService.verify_product_exists(data.product_id):
+            return None
+
+        new_review = Review(
+            product_id=data.product_id,
+            user_id=user_id,
+            text=data.text,
+            rating=data.rating,
+            status='active'
+        )
+        return ReviewRepository.create(new_review)
 
     @staticmethod
     def find_by_id(review_id: int) -> Optional[Review]:
-        ...
+        return Review.query.get(review_id)
 
     @staticmethod
     def find_by_product_id(product_id: int) -> List[Review]:
-        ...
+        return Review.query.filter_by(product_id=product_id, status='active').all()
 
     @staticmethod
     def get_by_user_id(user_id: int) -> List[Review]:
-        ...
+        return Review.query.filter_by(user_id=user_id).all()
 
     @staticmethod
-    def update_review(review_update_request: ReviewUpdateRequest, review_id: int, user_id: int) -> Review:
-        # в отличие от delete админ не должен уметь редактировать чужие отзывы
-        # получаем отзыв по его id, сверяем id пользователя, обновляем дынные
-        # id пользователя берем из токена, id отзыва из маршрута
-        # делаем Update запрос в репозиторий
-        ...
+    def update_review(data, review_id: int, user_id: int) -> Optional[Review]:
+        review = Review.query.get(review_id)
+        if not review or review.user_id != user_id or review.status == 'deleted':
+            return None
+
+        review.text = data.text
+        review.rating = data.rating
+        db.session.commit()
+
+        return review
+
 
     @classmethod
     def delete_by_id(cls, review_id: int, user_id: int, is_admin: bool) -> bool:
-        # true если удаление успешно, false если нет (отзыв не существует или уже удален)
-        # надо учесть что админ может удалять чужие отзывы
-        # передаем сюда, является ли пользователь админом и его id (все из токена берем в роутере)
-        # как и в update cначала получаем отзыв по id и сверяемся
-        ...
+        review = Review.query.get(review_id)
+        if not review or review.status == 'deleted':
+            return False
+
+        if is_admin or review.user_id == user_id:
+            review.status = 'deleted'
+            db.session.commit()
+            return True
+
+        return False
