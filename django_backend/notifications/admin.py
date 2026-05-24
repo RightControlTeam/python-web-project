526c90a6eb0458b3523ee0c6f0ab4a4e793839ad
DariaEFAL <EF.Daria.AL@yandex.ru>
2026-05-24 20:31:55 +0300

    fix admin in notifications

--- django_backend/notifications/admin.py
+++ django_backend/notifications/admin.py
@@ -1,2 +1,9 @@
 from django.contrib import admin
+from .models import Notification
 
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'notification_type', 'order_id', 'transaction_id', 'success', 'is_processed', 'created_at']
+    list_filter = ['notification_type', 'success', 'is_processed', 'created_at']
+    search_fields = ['order_id', 'transaction_id', 'message']
+    readonly_fields = ['created_at', 'processed_at']
